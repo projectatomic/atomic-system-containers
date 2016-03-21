85f72f4cfe2061fff59fc0a4eb04ae8b3926fbe0
Giuseppe Scrivano <gscrivan@redhat.com>
2016-03-21 09:31:00 +0100

    Initial commit

---
+++ etcd/etcd-env.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+
+ipaddress=$(hostname -I | cut -f1 -d' ')
+export ETCD_NAME=$HOSTNAME
+export ETCD_DATA_DIR=/var/lib/etcd/$HOSTNAME.etcd
+export ETCD_ADVERTISE_CLIENT_URLS=http://${ipaddress}:2379,http://${ipaddress}:4001
+export ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379,http://0.0.0.0:4001
+export ETCD_INITIAL_ADVERTISE_PEER_URLS=http://${ipaddress}:2380,http://${ipaddress}:7001
+export ETCD_LISTEN_PEER_URLS=http://0.0.0.0:2380,http://0.0.0.0:7001
+export ETCD_INITIAL_CLUSTER=$HOSTNAME=http://${ipaddress}:2380,$HOSTNAME=http://${ipaddress}:7001
+
+# Execute the commands passed to this script
+exec "$@"
