f3e4e6bfc1f9ea7a7e4ef839a23991bbe290dc3a
Giuseppe Scrivano <gscrivan@redhat.com>
2016-09-05 14:18:44 +0200

    etcd: do not use ETCD_DATA_DIR anymore
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- etcd/etcd-env.sh
+++ etcd/etcd-env.sh
@@ -5,7 +5,7 @@ if test x$NAME == x; then
     NAME=$HOSTNAME
 fi
 export ETCD_NAME=$HOSTNAME
-export ETCD_DATA_DIR=${ETCD_DATA_DIR:-/var/lib/etcd/${NAME}.etcd}
+export ETCD_DATA_DIR=/var/lib/etcd/${NAME}.etcd
 export ETCD_ADVERTISE_CLIENT_URLS=http://${ipaddress}:2379,http://${ipaddress}:4001
 export ETCD_LISTEN_CLIENT_URLS=http://0.0.0.0:2379,http://0.0.0.0:4001
 export ETCD_INITIAL_ADVERTISE_PEER_URLS=http://${ipaddress}:2380,http://${ipaddress}:7001
