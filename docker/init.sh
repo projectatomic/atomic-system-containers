3b319c437460cd5872f501555748deac1b33a7d1
Giuseppe Scrivano <gscrivan@redhat.com>
2016-11-06 20:05:56 +0100

    docker: call docker-storage-setup
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- docker/init.sh
+++ docker/init.sh
@@ -1,5 +1,11 @@
 #!/bin/sh
 
+# set storage first
+(
+    . /etc/sysconfig/docker-storage-setup
+    /usr/bin/docker-storage-setup
+)
+
 for i in /etc/sysconfig/{docker,docker-storage,docker-network}; do
     test -e $i && . $i
 done
