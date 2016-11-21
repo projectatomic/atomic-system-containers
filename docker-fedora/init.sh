c8046a5b0a7f583eb5eb413b45f67a397e766673
Giuseppe Scrivano <gscrivan@redhat.com>
2016-11-21 17:25:10 +0100

    docker-fedora: update to docker 1.12
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- docker-fedora/init.sh
+++ docker-fedora/init.sh
@@ -12,8 +12,19 @@ done
 
 export GOTRACEBACK=crash
 
-exec /usr/bin/docker daemon --exec-opt native.cgroupdriver=systemd \
-          $OPTIONS \
-          $DOCKER_STORAGE_OPTIONS \
-          $DOCKER_NETWORK_OPTIONS \
-          $INSECURE_REGISTRY
+/usr/libexec/docker/docker-containerd-current \
+    --listen unix:///run/containerd.sock      \
+    --shim /usr/libexec/docker/docker-containerd-shim-current &
+
+exec /usr/bin/dockerd-current \
+     --add-runtime oci=/usr/libexec/docker/docker-runc-current \
+     --default-runtime=oci \
+     --containerd /run/containerd.sock \
+     --exec-opt native.cgroupdriver=systemd \
+     --userland-proxy-path=/usr/libexec/docker/docker-proxy-current \
+     $OPTIONS \
+     $DOCKER_STORAGE_OPTIONS \
+     $DOCKER_NETWORK_OPTIONS \
+     $ADD_REGISTRY \
+     $BLOCK_REGISTRY \
+     $INSECURE_REGISTRY
