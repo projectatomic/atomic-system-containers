60ca92ac48b620a50709ffb3cbf6f07280e92add
Giuseppe Scrivano <gscrivan@redhat.com>
2016-12-19 16:40:04 +0100

    docker-centos: add support for docker plugins
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- docker-centos/init.sh
+++ docker-centos/init.sh
@@ -8,7 +8,18 @@
 
 source /run/docker-bash-env
 
+# Run all the installed containers
+mkdir -p /run/docker/plugins/
+ls -1 /usr/libexec/docker/*plugin |  \
+while read i;
+do
+    plugin=$(basename $i)
+    test -e /run/docker/plugins/$plugin.sock || mkfifo /run/docker/plugins/$plugin.sock
+    $i &
+done
+
 exec /usr/bin/docker-current daemon \
+          --authorization-plugin=rhel-push-plugin \
           $OPTIONS \
           $DOCKER_STORAGE_OPTIONS \
           $DOCKER_NETWORK_OPTIONS \
