5b015a287fc1adc046a97e0200246264ade9866f
Giuseppe Scrivano <gscrivan@redhat.com>
2017-03-03 18:11:26 +0100

    Revert "docker-centos: use a chroot environment"
    This reverts commit 592263adc293c59366e4e56920e3dc527f3e36d2.

--- docker-centos/init.sh
+++ docker-centos/init.sh
@@ -8,11 +8,7 @@
 
 source /run/docker-bash-env
 
-exec /usr/bin/dockerd-current \
-          --add-runtime docker-runc=/usr/libexec/docker/docker-runc-current \
-          --default-runtime=docker-runc \
-          --exec-opt native.cgroupdriver=systemd \
-          --userland-proxy-path=/usr/libexec/docker/docker-proxy-current \
+exec /usr/bin/docker-current daemon \
           $OPTIONS \
           $DOCKER_STORAGE_OPTIONS \
           $DOCKER_NETWORK_OPTIONS \
