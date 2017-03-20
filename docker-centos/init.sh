c9208c2fae0534d200087df718bd36fa5cb887f2
Giuseppe Scrivano <gscrivan@redhat.com>
2017-03-20 10:54:33 +0100

    docker-centos: add support for plugins
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- docker-centos/init.sh
+++ docker-centos/init.sh
@@ -8,6 +8,16 @@
 
 source /run/docker-bash-env
 
+# Run all the installed containers
+mkdir -p /run/docker/plugins/
+ls -1 /usr/libexec/docker/*plugin |  \
+while read i;
+do
+    plugin=$(basename $i)
+    test -e /run/docker/plugins/$plugin.sock || mkfifo /run/docker/plugins/$plugin.sock
+    $i &
+done
+
 exec /usr/bin/docker-current daemon \
           --exec-opt native.cgroupdriver=systemd \
           $OPTIONS \
