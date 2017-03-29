690cbd9c2d596883d75f6e82ddf52fc135a7d9f6
Giuseppe Scrivano <gscrivan@redhat.com>
2017-03-29 17:40:52 +0200

    docker-centos: hardcode some binaries paths
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>
    
    Closes: #51
    Approved by: giuseppe

--- docker-centos/init.sh
+++ docker-centos/init.sh
@@ -18,8 +18,22 @@ do
     $i &
 done
 
+/usr/bin/docker-containerd-current \
+    --listen unix:///run/containerd.sock \
+    --shim /usr/bin/docker-containerd-shim-current \
+    --start-timeout 2m &
+
+while test \! -e /run/containerd.sock;
+do
+      sleep 0.1
+done
+
 exec /usr/bin/docker-current daemon \
+          --add-runtime oci=/usr/libexec/docker/docker-runc-current \
+          --default-runtime=oci \
+          --containerd /run/containerd.sock \
           --exec-opt native.cgroupdriver=systemd \
+          --userland-proxy-path=/usr/libexec/docker/docker-proxy-current \
           $OPTIONS \
           $DOCKER_STORAGE_OPTIONS \
           $DOCKER_NETWORK_OPTIONS \
