3953aef6238b9743d7435bab7c2da50d0dfe181b
Giuseppe Scrivano <gscrivan@redhat.com>
2017-04-28 19:05:24 +0200

    docker-fedora: use /etc/docker/daemon.json as docker-centos
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- docker-fedora/init.sh
+++ docker-fedora/init.sh
@@ -30,11 +30,7 @@ do
 done
 
 exec /usr/bin/dockerd-current \
-     --add-runtime oci=/usr/libexec/docker/docker-runc-current \
-     --default-runtime=oci \
-     --containerd /run/containerd.sock \
-     --exec-opt native.cgroupdriver=systemd \
-     --userland-proxy-path=/usr/libexec/docker/docker-proxy-current \
+     --config-file=/etc/docker/daemon.json \
      $OPTIONS \
      $DOCKER_STORAGE_OPTIONS \
      $DOCKER_NETWORK_OPTIONS \
