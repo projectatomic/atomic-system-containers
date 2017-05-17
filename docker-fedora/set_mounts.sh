aa6851b6dd6e7e9d6b0ba9819afdd9a8664c1ede
Giuseppe Scrivano <gscrivan@redhat.com>
2017-05-17 18:41:20 +0200

    docker-fedora: add rslave bind mount for /run/systemd
    so that the runC notify socket is not propagated to the host.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- docker-fedora/set_mounts.sh
+++ docker-fedora/set_mounts.sh
@@ -2,3 +2,4 @@
 
 findmnt /var/lib > /dev/null || mount --bind --make-shared /var/lib /var/lib
 mount --make-shared /run
+findmnt /run/systemd > /dev/null || mount --bind --make-rslave /run/systemd /run/systemd
