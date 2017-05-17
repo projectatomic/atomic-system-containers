2b47e57fbbc9a26062ae28f2b0af9400113a663b
Giuseppe Scrivano <gscrivan@redhat.com>
2017-05-17 09:18:23 +0200

    fixup! docker-fedora: use sd-notify to signal Docker is ready

--- docker-fedora/init.sh
+++ docker-fedora/init.sh
@@ -10,6 +10,9 @@ source /run/docker-bash-env
 
 getent group docker || groupadd docker
 
+
+# Inhibit sd-notify for docker-containerd, we want to get the notification
+# from the docker process
 NOTIFY_SOCKET=/dev/null /usr/libexec/docker/docker-containerd-current \
     --listen unix:///run/containerd.sock      \
     --shim /usr/bin/shim.sh &
