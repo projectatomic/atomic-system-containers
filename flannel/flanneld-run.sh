109f393a52ecb6390cbfa794ddaff25ef60c81ba
Jason Brooks <jbrooks@redhat.com>
2017-06-19 17:30:12 -0700

    flannel tweaks for running with kube ansible
    * add cap for reading cert dir
    * mount config/certs dirs
    * copy and source config file from rpm

--- flannel/flanneld-run.sh
+++ flannel/flanneld-run.sh
@@ -4,10 +4,12 @@
 echo "[Service]" > /etc/systemd/system/docker.service.d/$NAME.conf
 echo "EnvironmentFile=-/run/$NAME/docker" >> /etc/systemd/system/docker.service.d/$NAME.conf
 
+source /etc/sysconfig/flanneld
+
 # Ensure this file doesn't already exist.
 rm -f run/flannel/subnet.env
 
-NOTIFY_SOCKET=/dev/null /usr/bin/flanneld &
+NOTIFY_SOCKET=/dev/null /usr/bin/flanneld -etcd-endpoints=${FLANNEL_ETCD_ENDPOINTS} -etcd-prefix=${FLANNEL_ETCD_PREFIX} -etcd-cafile=${FLANNEL_ETCD_CAFILE} -etcd-certfile=${FLANNEL_ETCD_CERTFILE} -etcd-keyfile=${FLANNEL_ETCD_KEYFILE} $FLANNEL_OPTIONS &
 child=$!
 
 while test \! -e /run/flannel/subnet.env
