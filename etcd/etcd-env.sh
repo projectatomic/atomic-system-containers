8624036cf77453a29505f48b1eeeb12d6af85632
Jason Brooks <jbrooks@redhat.com>
2017-06-22 08:53:01 -0700

    allow for multiple etcd installs

--- etcd/etcd-env.sh
+++ etcd/etcd-env.sh
@@ -7,9 +7,6 @@ fi
 export ETCD_NAME=${ETCD_NAME:-$HOSTNAME}
 export ETCD_DATA_DIR=/var/lib/etcd/${NAME}.etcd
 
-sed -i s/ETCD_NAME.*/ETCD_NAME=$ETCD_NAME/g /etc/etcd/etcd.conf
-sed -i s/ETCD_DATA_DIR.*/ETCD_DATA_DIR=$ETCD_DATA_DIR/g /etc/etcd/etcd.conf
-
 source /etc/etcd/etcd.conf
 
 # Execute the commands passed to this script
