0c68898bf256011fabee1a93f45eef9745e1afa6
Giuseppe Scrivano <gscrivan@redhat.com>
2017-08-04 13:22:21 +0200

    cri-o-fedora: add mount point for /var/lib/origin
    Having the rootfs under /var/lib and using the shared propagation
    prevent to have the right propagation for /var/lib/origin.  Add a mount
    point to ensure it is the same as on the host.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- cri-o-fedora/set_mounts.sh
+++ cri-o-fedora/set_mounts.sh
@@ -1,5 +1,6 @@
 #!/bin/sh
 
 findmnt /var/lib > /dev/null || mount --bind --make-shared /var/lib /var/lib
+findmnt /var/lib/origin > /dev/null || mount --bind --make-shared /var/lib/origin /var/lib/origin
 mount --make-shared /run
 findmnt /run/systemd > /dev/null || mount --bind --make-rslave /run/systemd /run/systemd
