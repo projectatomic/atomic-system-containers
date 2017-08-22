e62b9eccdf75cd23374d2c5d2dc57ec8713a8dbc
Giuseppe Scrivano <gscrivan@redhat.com>
2017-08-22 12:10:00 +0200

    test.sh: use the preprocessed config.json.template file
    The JSON file could be different after replacing the default
    values in the manifest.json file.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- test.sh
+++ test.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 
 find . -name 'config.json*' -o -name 'manifest.json' | while read i; do
     echo Testing file $i
-    json_verify -s < $i
+    MODE=PREPROCESS lint/syscontainers-lint $(dirname $i) $i | json_verify -s
 done
 
 find . -name Dockerfile -exec dirname \{\} \; | while read i; do
