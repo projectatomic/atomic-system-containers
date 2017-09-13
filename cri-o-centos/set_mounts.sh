5fed63e1d073b3be500cf931779b3188f0be315f
Giuseppe Scrivano <gscrivan@redhat.com>
2017-09-13 18:35:44 +0200

    cri-o-centos: prevent a bind mounts bomb
    Under RHELAH I've seen that when cri-o is restarted a few times, we
    create a lot of mount points.  That is because we are bind mounting as
    rshared both /var/lib and /var/lib/origin.  We needed /var/lib because
    we want to share bind mounts from /var/lib/containers.  Solve it by
    creating a new bind mount and make /var/lib private.
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>
    
    Closes: #113
    Approved by: ashcrow

--- cri-o-centos/set_mounts.sh
+++ cri-o-centos/set_mounts.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
 findmnt /var/lib > /dev/null || mount --bind --make-shared /var/lib /var/lib
+findmnt /var/lib/containers > /dev/null || mount --bind --make-shared /var/lib/containers /var/lib/containers
 findmnt /var/lib/origin > /dev/null || mount --bind --make-shared /var/lib/origin /var/lib/origin
 mount --make-shared /run
 findmnt /run/systemd > /dev/null || mount --bind --make-rslave /run/systemd /run/systemd
