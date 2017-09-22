9690159516d332e9f5bc975c3bccaf43df2955fe
Giuseppe Scrivano <gscrivan@redhat.com>
2017-09-23 00:58:26 +0200

    docker-fedora: rbind /var/lib
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- docker-fedora/set_mounts.sh
+++ docker-fedora/set_mounts.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 
-findmnt /var/lib > /dev/null || mount --bind --make-shared /var/lib /var/lib
+findmnt /var/lib > /dev/null || mount --rbind --make-shared /var/lib /var/lib
 mount --make-shared /run
 findmnt /run/systemd > /dev/null || mount --bind --make-rslave /run/systemd /run/systemd
