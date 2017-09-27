e219ec6f52797b305a29761dcc0668d7d54c39df
Giuseppe Scrivano <gscrivan@redhat.com>
2017-09-27 17:10:04 +0200

    cri-o-centos: restrict bind mount to /var/lib/containers/storage
    Do not interfere with the OSTree storage under /var/lib/containers/atomic
    
    Signed-off-by: Giuseppe Scrivano <gscrivan@redhat.com>

--- cri-o-centos/set_mounts.sh
+++ cri-o-centos/set_mounts.sh
@@ -1,6 +1,6 @@
 #!/bin/sh
 
-findmnt /var/lib/containers > /dev/null || mount --bind --make-shared /var/lib/containers /var/lib/containers
+findmnt /var/lib/containers/storage > /dev/null || mount --bind --make-shared /var/lib/containers/storage /var/lib/containers/storage
 findmnt /var/lib/origin > /dev/null || mount --bind --make-shared /var/lib/origin /var/lib/origin
 mount --make-shared /run
 findmnt /run/systemd > /dev/null || mount --bind --make-rslave /run/systemd /run/systemd
